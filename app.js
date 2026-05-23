5a8792c0f4139a16559dec7d3d43d522c519be3f
oceanvacationsmb <oceanvacationsmb@gmail.com>
2026-05-22 23:46:33 -0400

    Add button click event handlers in app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("NokSourdough app connected");
+document.getElementById("addCustomerBtn").onclick = function () {
+  alert("Add Customer clicked");
+};
+
+document.getElementById("addProductBtn").onclick = function () {
+  alert("Add Product clicked");
+};
+
+document.getElementById("createInvoiceBtn").onclick = function () {
+  alert("Create Invoice clicked");
+};
+
+document.getElementById("exportPdfBtn").onclick = function () {
+  window.print();
+};
